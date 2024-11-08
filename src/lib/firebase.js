7eb53bd26c638cc3db917d8c3e2bc4557bf69722
Cristian Machado <cris6mc@gmail.com>
2024-11-08 11:37:02 -0500

    Refactor Firebase configuration to use environment variables and update .gitignore for local env files

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,15 +8,27 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDaDUPZiYwatYZhFpN7C9i77ntvxTRlJQY",
+//     authDomain: "san-pedro-de-yauyo-50eea.firebaseapp.com",
+//     projectId: "san-pedro-de-yauyo-50eea",
+//     storageBucket: "san-pedro-de-yauyo-50eea.appspot.com",
+//     messagingSenderId: "506398475293",
+//     appId: "1:506398475293:web:fd6a3e2f431e07d6ad32f4",
+//     measurementId: "G-6VK11HLSVF"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDaDUPZiYwatYZhFpN7C9i77ntvxTRlJQY",
-    authDomain: "san-pedro-de-yauyo-50eea.firebaseapp.com",
-    projectId: "san-pedro-de-yauyo-50eea",
-    storageBucket: "san-pedro-de-yauyo-50eea.appspot.com",
-    messagingSenderId: "506398475293",
-    appId: "1:506398475293:web:fd6a3e2f431e07d6ad32f4",
-    measurementId: "G-6VK11HLSVF"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
