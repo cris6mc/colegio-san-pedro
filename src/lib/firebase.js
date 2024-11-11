bddac7bbefa1adda48c866323ad351090528066d
AlexeiEvenepoel <e_2019200797H@uncp.edu.pe>
2024-11-11 09:29:33 -0500

    api puesta de nuevo en firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,15 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDaDUPZiYwatYZhFpN7C9i77ntvxTRlJQY",
+  authDomain: "san-pedro-de-yauyo-50eea.firebaseapp.com",
+  projectId: "san-pedro-de-yauyo-50eea",
+  storageBucket: "san-pedro-de-yauyo-50eea.appspot.com",
+  messagingSenderId: "506398475293",
+  appId: "1:506398475293:web:fd6a3e2f431e07d6ad32f4",
+  measurementId: "G-6VK11HLSVF",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
